bf82f0a9d63f6e64d68bb098fab74d079eef67cc
Your Name <xiaohd3758@163.com>
2020-06-23 13:20:15 +0800

    "第一次提交代码"

---
+++ src/main/java/com/igeek/demo1.java
@@ -0,0 +1,11 @@
+package com.igeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class demo1 {
+    public static void main(String[] args) {
+        SpringApplication.run(demo1.class,args);
+    }
+}
